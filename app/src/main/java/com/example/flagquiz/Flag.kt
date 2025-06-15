3e719e9bd279f2971895550c4b87e076aff2dfd2
Halil Akgun <halilakgun0@gmail.com>
2025-06-15 22:47:02 +0200

    Initial commit

---
+++ app/src/main/java/com/example/flagquiz/Flag.kt
@@ -0,0 +1,4 @@
+package com.example.flagquiz
+
+data class Flag(var id: Int, var name: String, var image: String) {
+}
---
+++ app/src/main/java/com/example/flagquiz/ResultActivity.kt
@@ -0,0 +1,32 @@
+package com.example.flagquiz
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.flagquiz.databinding.ActivityResultBinding
+
+class ResultActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityResultBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityResultBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.buttonPlayAgain.setOnClickListener {
+            val intent = Intent(this, QuizActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
